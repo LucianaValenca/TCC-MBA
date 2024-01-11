6a489ccec0e955543ca090336efa26bdf16f6a4b
LucianaValenca <137066275+LucianaValenca@users.noreply.github.com>
2024-01-11 17:30:43 -0300

    Add files via upload

---
+++ SCRIPT - TCC-Transferências.R
@@ -0,0 +1,5 @@
+source("SCRIPT - Instalação de Pacotes.R", local = FALSE)
+source("SCRIPT - Leitura de Planilhas.R", local = FALSE, echo=TRUE)
+source("SCRIPT- Preparação Dados.R", local = FALSE, echo=TRUE)
+source("SCRIPT - Estatísticas.R", local = FALSE, echo=TRUE)
+
